9986c108f02d7800efd9ae3cbedc4d4d75a1af50
priyamukherjee468 <117980359+priyamukherjee468@users.noreply.github.com>
2023-06-11 21:21:42 +0530

    Update MailMetadata.java

--- MailMetadata.java
+++ MailMetadata.java
@@ -6,8 +6,8 @@ public class MailMetadata {
     public static final String sslProperty="mail.smtp.ssl.enable";
     public static final String authPerm="mail.smtp.auth";
     public static final String myUserMail="priyamukherjee468@gmail.com";
-    public static final String password="yhdeopqhtweatdrn";
-    public static final String receiverMail="jayantanandy001@gmail.com";
+    public static final String password="==";
+    public static final String receiverMail=" ";
 
 
 
